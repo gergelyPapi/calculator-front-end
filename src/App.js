fed86b07a410c747fbbb42e8d18d4929f41bc3ad
Gergely Papi <gergely.papi.cc@gmail.com>
2022-08-21 23:06:45 +0200

    Fixed number order

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Button from "./components/Button";
 
 const btnValues = [
   ["X", "/", "+", "-"],
-  [7, 8, 9,],
-  [4, 5, 6,],
   [1, 2, 3,],
+  [4, 5, 6,],
+  [7, 8, 9,],
   [0, ".", "="],
   ["Load", "Save"],
 ];
