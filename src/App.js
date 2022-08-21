eeccf82b3b256bf8890e29d0bc05610e2cf5a703
Gergely Papi <gergely.papi.cc@gmail.com>
2022-08-21 20:31:30 +0200

    Wired in buttons into App.js, fixed typos in readme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import './App.css';
 
+import Frame from "./components/Frame";
+import Screen from "./components/Screen";
+import ButtonFrame from "./components/ButtonFrame";
+import Button from "./components/Button";
+
+const btnValues = [
+  ["X", "/", "+", "-"],
+  [7, 8, 9,],
+  [4, 5, 6,],
+  [1, 2, 3,],
+  [0, ".", "="],
+  ["Load", "Save"],
+];
+
 function App() {
   return (
-    <div>
-    
-    </div>
+    <Frame>
+      <Screen value='0'/>
+      <ButtonFrame>
+        {
+          btnValues.flat().map((btn, i) => {
+            let className;
+            if(btn === "=") {
+              className = "equals";
+            } else if (btn === "Save" || btn === "Load") {
+              className = "split";
+            } else {
+              className = "";
+            }
+            return (
+              <Button
+                key={i}
+                className={className}
+                value={btn}
+                onClick={() => {
+                  
+                }}
+              />
+            );
+          })
+        }
+      </ButtonFrame>
+    </Frame>
   );
 }
 
